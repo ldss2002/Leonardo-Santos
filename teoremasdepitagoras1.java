c14683a242c75d8e81e9a45850174d5c38a9951e
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2017-11-09 17:05:06 +0000

    Add files via upload

---
+++ teoremasdepitagoras1.java
@@ -0,0 +1,19 @@
+package Teoremadepitagoras;
+
+import java.util.Scanner;
+
+public class teoremasdepitagoras1 private static final char[] Calcule = null;
+
+}
+
+	public static void main(String[] args) {
+		Scanner dados = new Scanner (System.in);
+		System.out.println(Calcule Object cateto;
+		o primeiro cateto);
+		
+		;
+	}
+}
+
+	
+	
