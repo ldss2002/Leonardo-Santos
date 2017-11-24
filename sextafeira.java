93da3de7aa68010cf52976fdba2fa440b9c0bac7
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2017-11-24 15:02:49 +0000

    Add files via upload

---
+++ sextafeira.java
@@ -0,0 +1,11 @@
+
+class sextafeira {
+ 
+	public static void main(String[] args) {
+		String message = "Hoje é sexta-feira";
+		for(int I=1; I <=5; I++)
+			System.out.println(message);
+		}
+	}
+
+
