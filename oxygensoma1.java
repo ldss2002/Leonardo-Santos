6fa0c3e266ceae397527953e0083cea22c9217bf
ldss2002 <32168991+ldss2002@users.noreply.github.com>
2017-11-24 15:52:09 +0000

    Add files via upload

---
+++ oxygensoma1.java
@@ -0,0 +1,21 @@
+package oxygensoma;
+import java.util.Scanner;
+public class oxygensoma1 {
+
+		public static void main(String[] args) {
+			int n1=1, i, n2;
+			Scanner dados = new Scanner (System.in);
+					
+					System.out.print("Numero:");
+					n2 = dados.nextInt();
+			for ( i=2; i<=n2; i++)
+				n1=n1+i;
+			System.out.print("O resultado dos primeiros positivos de "+n2+" é: " + n1);
+				
+			
+		
+
+	}
+	}
+
+
